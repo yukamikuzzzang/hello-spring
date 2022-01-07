627a1233d9abd6ca0fa1bd407a2d23b294dce1a0
SYS <yukamikuzzzang@gmail.com>
2022-01-07 18:29:05 +0900

    220107_9강까지

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,50 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {           //스프링이 만든 모델이 들어감.
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model) {    //required = true(default = true)
+        model.addAttribute("name", name);    //키 값 name, Value : 입력한 name의 값
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;  //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;    //key
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
+
